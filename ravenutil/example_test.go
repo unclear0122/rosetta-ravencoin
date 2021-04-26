8cf1fd345622a210e54bb9e97f9b821449bce22a
unclear0122 <39553384+unclear0122@users.noreply.github.com>
2021-04-26 22:17:05 +0200

    Added ravenutil, like for real this time.
    * adding --disable-pruning switch
    
    * ravenutil, first try for construction API
    
    * ravenutil

---
+++ ravenutil/example_test.go
@@ -0,0 +1,76 @@
+package ravenutil_test
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/RavenProject/rosetta-ravencoin/ravenutil"
+)
+
+func ExampleAmount() {
+
+	a := ravenutil.Amount(0)
+	fmt.Println("Zero Satoshi:", a)
+
+	a = ravenutil.Amount(1e8)
+	fmt.Println("100,000,000 Satoshis:", a)
+
+	a = ravenutil.Amount(1e5)
+	fmt.Println("100,000 Satoshis:", a)
+	// Output:
+	// Zero Satoshi: 0 BTC
+	// 100,000,000 Satoshis: 1 BTC
+	// 100,000 Satoshis: 0.001 BTC
+}
+
+func ExampleNewAmount() {
+	amountOne, err := ravenutil.NewAmount(1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(amountOne) //Output 1
+
+	amountFraction, err := ravenutil.NewAmount(0.01234567)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(amountFraction) //Output 2
+
+	amountZero, err := ravenutil.NewAmount(0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(amountZero) //Output 3
+
+	amountNaN, err := ravenutil.NewAmount(math.NaN())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(amountNaN) //Output 4
+
+	// Output: 1 BTC
+	// 0.01234567 BTC
+	// 0 BTC
+	// invalid bitcoin amount
+}
+
+func ExampleAmount_unitConversions() {
+	amount := ravenutil.Amount(44433322211100)
+
+	fmt.Println("Satoshi to kBTC:", amount.Format(ravenutil.AmountKiloBTC))
+	fmt.Println("Satoshi to BTC:", amount)
+	fmt.Println("Satoshi to MilliBTC:", amount.Format(ravenutil.AmountMilliBTC))
+	fmt.Println("Satoshi to MicroBTC:", amount.Format(ravenutil.AmountMicroBTC))
+	fmt.Println("Satoshi to Satoshi:", amount.Format(ravenutil.AmountSatoshi))
+
+	// Output:
+	// Satoshi to kBTC: 444.333222111 kBTC
+	// Satoshi to BTC: 444333.222111 BTC
+	// Satoshi to MilliBTC: 444333222.111 mBTC
+	// Satoshi to MicroBTC: 444333222111 μBTC
+	// Satoshi to Satoshi: 44433322211100 Satoshi
+}
